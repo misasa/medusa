4a4460165c2004d3d58f2b2f310d7e8f2baa255d
Probizmo <probizmo@localhost.localdomain>
2014-01-06 20:23:31 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medusa::Application.config.secret_key_base = '823eaaf6ab8c63168c54626e3fe8dcc28b502d7ad1774f606b9b57ca044d03aee3180aa0299efbc2c1225dfbfccdc86aeab380bc5b15a972bf60e08b949f28ae'
