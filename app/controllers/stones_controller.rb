b846a634ed6848494850cd9387bf2e82d0b230cb
m-imaoka <m-imaoka@probizmo.co.jp>
2014-01-10 16:22:53 +0900

    ソートおよびページネーションのサンプルを実装

--- app/controllers/stones_controller.rb
+++ app/controllers/stones_controller.rb
@@ -1,8 +1,9 @@
 class StonesController < ApplicationController
 
   def index
-    @q = Stone.search(params[:q])
-    @stones = @q.result(distinct: true)
+    @search = Stone.search(params[:q])
+    @search.sorts = "updated_at ASC" if @search.sorts.empty?
+    @stones = @search.result.page(params[:page]).per(params[:per_page])
   end
 
 end
