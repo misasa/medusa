60fe42315a3316ab365095fcb2743429eb3b2fb3
m-imaoka <m-imaoka@probizmo.co.jp>
2014-01-10 17:47:24 +0900

    新規作成のサンプルを実装

--- app/controllers/stones_controller.rb
+++ app/controllers/stones_controller.rb
@@ -6,4 +6,24 @@ class StonesController < ApplicationController
     @stones = @search.result.page(params[:page]).per(params[:per_page])
   end
 
+  def create
+    @stone = Stone.new(stone_params)
+    if @stone.save
+      redirect_to stones_path
+    else
+      # TODO: When validation error .....
+    end
+  end
+
+  private
+
+  def stone_params
+    params.require(:stone).permit(
+      :name,
+      :physical_form_id,
+      :classification_id,
+      :tag_list
+    )
+  end
+
 end
