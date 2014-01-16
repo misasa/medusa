edad6f5edd265a29b103ba33b9bb1b10e780dc69
Toshiki Ikeuchi <t-ikeuchi@probizmo.co.jp>
2014-01-16 18:17:32 +0900

    referringモデルの関連名を修正

--- app/models/referring.rb
+++ app/models/referring.rb
@@ -1,6 +1,6 @@
 class Referring < ActiveRecord::Base
-  belongs_to :bibs
+  belongs_to :bib
   belongs_to :referable, polymorphic: true
 
-  validates :bibs, existence: true
+  validates :bib, existence: true
 end
