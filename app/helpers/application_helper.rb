fb35a94559781b472d1c14693d666e9e97548ac9
Kentaro Matsukawa <k-matsukawa@probizmo.co.jp>
2014-01-17 14:10:20 +0900

    group一覧の表示機能を実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,26 @@ module ApplicationHelper
     system_modes.include?(controller_name) ? "layouts/navbar_system" : "layouts/navbar_default"
   end
 
+  def difference_from_now(time)
+    return unless time
+    now = Time.now
+    sec = now - time
+    today_in_sec = now - now.at_beginning_of_day
+    yesterday_in_sec = now - 1.days.ago.at_beginning_of_day
+
+    if sec <= today_in_sec
+      if sec < 60
+        "#{sec.floor} s ago"
+      elsif sec < (60*60)
+        "#{(sec / 60).floor} m ago"
+      elsif sec < (60*60*24)
+        "#{(sec / (60*60)).floor} h ago"
+      end
+    elsif (today_in_sec < sec) && (sec < yesterday_in_sec)
+      "yesterday, #{time.hour}:#{time.min}"
+    else
+      time.to_date
+    end
+  end
+
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -70,4 +70,46 @@ describe ApplicationHelper do
     end
   end
 
+  describe "#difference_from_now" do
+    subject { helper.difference_from_now(time) }
+    before { allow(Time).to receive(:now).and_return(now) }
+    let(:now) { Time.local(2014,1,1,12,0,0) }
+    context "time is nil" do
+      let(:time) { nil }
+      it { expect(subject).to be_nil }
+    end
+    context "time equal now" do
+      let(:time) { now }
+      it { expect(subject).to eq "0 s ago" }
+    end
+    context "time is 59 sec ago" do
+      let(:time) { now - 59 }
+      it { expect(subject).to eq "59 s ago" }
+    end
+    context "time is 60 sec ago" do
+      let(:time) { now - 60 }
+      it { expect(subject).to eq "1 m ago" }
+    end
+    context "time is (59m and 59s) ago" do
+      let(:time) { now - ((60*60)-1) }
+      it { expect(subject).to eq "59 m ago" }
+    end
+    context "time is 60 min ago" do
+      let(:time) { now - (60*60) }
+      it { expect(subject).to eq "1 h ago" }
+    end
+    context "time is 12 hour ago" do
+      let(:time) { now - (60*60*12) }
+      it { expect(subject).to eq "12 h ago" }
+    end
+    context "time is (12h and 1s) ago" do
+      let(:time) { now - ((60*60*12)+1) }
+      it { expect(subject).to eq "yesterday, 23:59" }
+    end
+    context "time is 27 hour ago" do
+      let(:time) { now - ((60*60*27)) }
+      it { expect(subject).to eq time.to_date }
+    end
+  end
+
 end
