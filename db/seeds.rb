3ac15761aafd85b5c5c079853218159893ce1950
Toshiki Ikeuchi <t-ikeuchi@probizmo.co.jp>
2014-01-21 16:44:49 +0900

    rake db:seedが動作するようにseeds.rbを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+work_dir = Pathname.new("/tmp/medusa_csv_files")
+FileUtils.mkdir_p(work_dir)
+
+csv_dir = Rails.root.join("db", "csvs")
+array_csv = Pathname.glob(csv_dir.join("*.csv"))
+FileUtils.cp(array_csv, work_dir)
+
+Pathname.glob(work_dir.join("*.csv")) do |path|
+  ActiveRecord::Base.connection.execute("COPY #{path.basename(".csv")} FROM '#{path}' WITH CSV HEADER")
+end
+
+FileUtils.rm_r(work_dir)
