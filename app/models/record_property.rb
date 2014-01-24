0e41bc2aaf95f679e23de46afbd2a62babcb7395
Kentaro Matsukawa <k-matsukawa@probizmo.co.jp>
2014-01-24 11:43:12 +0900

    uniq_qrsテーブルをglobal_qrsに名前変更

--- app/models/record_property.rb
+++ app/models/record_property.rb
@@ -2,7 +2,7 @@ class RecordProperty < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
   belongs_to :datum, polymorphic: true
-  has_one :uniq_qr
+  has_one :global_qr
 
   validates :user, existence: true
   validates :group, existence: true
