c63dfadc31f66400630379e640167e2267f7d452
Kentaro Matsukawa <k-matsukawa@probizmo.co.jp>
2014-02-10 09:48:01 +0900

    cancan利用時に新規作成エラーが発生する問題に対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,12 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action do
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
   skip_before_filter :verify_authenticity_token # allow CSRF
-  
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
