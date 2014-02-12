90241c04337612eacf65aefb85f3bc10a7670166
m-watanabe <m-watanabe@probizmo.co.jp>
2014-02-12 18:21:33 +0900

    after_create create_record_propertyのテストコード追加

--- spec/models/concerns/has_record_property_spec.rb
+++ spec/models/concerns/has_record_property_spec.rb
@@ -119,4 +119,15 @@ describe HasRecordProperty do
       end
     end
   end
+  
+  describe "after_create create_record_property" do
+    let(:stone) { FactoryGirl.create(:stone)}
+    let(:user) { FactoryGirl.create(:user) }
+    it "" do
+      User.current = user
+      stone.save
+      expect(stone.record_property).not_to be_nil
+      expect(stone.record_property.persisted?).to be true
+    end
+  end
 end
