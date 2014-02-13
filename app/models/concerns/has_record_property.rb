c8ee2b96f7b87314db2a505c40c2b31505b95456
Kentaro Matsukawa <k-matsukawa@probizmo.co.jp>
2014-02-13 18:20:13 +0900

    stone一覧検索の実装(タグ検索のみ仮置き)

--- app/models/concerns/has_record_property.rb
+++ app/models/concerns/has_record_property.rb
@@ -3,8 +3,10 @@ module HasRecordProperty
 
   included do
     has_one :record_property, as: :datum
+    has_one :user, through: :record_property
+    has_one :group, through: :record_property
     accepts_nested_attributes_for :record_property
-    delegate :global_id, :readable?, to: :record_property
+    delegate :global_id, :published, :published_at, :readable?, to: :record_property
 
     after_create :generate_record_property
 
@@ -19,13 +21,13 @@ module HasRecordProperty
   def writable?(user)
     new_record? || record_property.writable?(user)
   end
-  
- def generate_record_property
+
+  def generate_record_property
     self.build_record_property unless self.record_property
     self.record_property.user_id = User.current.id unless User.current.nil?
     self.record_property.save
   end
-  
+
   module ClassMethods
     private
 
