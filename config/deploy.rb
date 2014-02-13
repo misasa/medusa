1c757c983eb353d2f59e0473bd89ede97b2bc5e0
m-imaoka <m-imaoka@probizmo.co.jp>
2014-02-13 16:25:21 +0900

    unicornの起動設定を追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :deploy_to, '/home/medusa/apps/'
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -40,8 +40,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'unicorn:restart'
     end
   end
 
---
+++ config/unicorn/production.rb
@@ -0,0 +1,30 @@
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 60
+preload_app true
+
+listen 3000
+pid File.expand_path("tmp/pids/unicorn.pid", ENV['RAILS_ROOT'])
+
+stderr_path File.expand_path('log/unicorn.error.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+
+before_fork do |server, worker|
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
