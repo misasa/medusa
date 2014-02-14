13b9ad9d2ab62cf86f00f3f6c474b93dbdf2acd6
m-watanabe <m-watanabe@probizmo.co.jp>
2014-02-14 16:41:42 +0900

    パス名間違い修正

--- spec/models/attachment_file_spec.rb
+++ spec/models/attachment_file_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 describe "AttachmentFile" do
   describe "Path" do
     let(:attachment_file){ FactoryGirl.create(:attachment_file)}
-    it{expect(attachment_file.path).to eq("/system/attachment_file/" + attachment_file.id.to_s + "/" + attachment_file.data_file_name)}
+    it{expect(attachment_file.path).to eq("/system/attachment_files/" + attachment_file.id.to_s + "/" + attachment_file.data_file_name)}
   end
 end
