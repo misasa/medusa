b914a71b095cfed2d30ed7bb3db7961b34714889
m-watanabe <m-watanabe@probizmo.co.jp>
2014-02-14 11:50:54 +0900

    Classification管理画面　classificationのfull_name自動設定処理追加

--- spec/factories/classifications.rb
+++ spec/factories/classifications.rb
@@ -7,4 +7,22 @@ FactoryGirl.define do
     lft 1
     rgt 1
   end
+  factory :classification_parent,class: Classification do
+    name "parent"
+    full_name ""
+    description "parent description"
+    parent_id ""
+  end
+  factory :classification_child,class: Classification do
+    name "child"
+    full_name ""
+    description "child description"
+    parent_id ""
+  end
+  factory :classification_grandchild,class: Classification do
+    name "grandchild"
+    full_name ""
+    description "grandchild description"
+    parent_id ""
+  end
 end
--- spec/models/classification_spec.rb
+++ spec/models/classification_spec.rb
@@ -24,4 +24,50 @@ describe Classification do
     end
   end
 
+  describe "full_name" do
+    let(:parent){ FactoryGirl.build(:classification_parent)}
+    let(:child){ FactoryGirl.build(:classification_child)}
+    let(:grandchild){ FactoryGirl.build(:classification_grandchild)}
+    context "new parent full_name" do
+      before{parent.save}
+      it{expect(parent).to be_persisted}
+      it{expect(parent.full_name).to eq parent.name}
+    end
+    context "new child full_name" do
+      before do
+        parent.save
+        child.parent_id = parent.id
+        child.save
+      end
+      it{expect(child).to be_persisted}
+      it{expect(child.full_name).to eq(parent.full_name + ":" + child.name)}
+    end
+    context "new grandchild full_name" do
+      before do
+        parent.save
+        child.parent_id = parent.id
+        child.save
+        grandchild.parent_id = child.id
+        grandchild.save
+      end
+      it{expect(grandchild).to be_persisted}
+      it{expect(grandchild.full_name).to eq(child.full_name + ":" + grandchild.name)}
+    end
+    context "change parent name" do
+      before do
+        parent.save
+        child.parent_id = parent.id
+        child.save
+        grandchild.parent_id = child.id
+        grandchild.save
+        parent.name = "parent2"
+        parent.save
+        child.reload
+        grandchild.reload
+      end
+      it{expect(parent.full_name).to eq parent.name}
+      it{expect(child.full_name).to eq(parent.full_name + ":" + child.name)}
+      it{expect(grandchild.full_name).to eq(child.full_name + ":" + grandchild.name)}
+    end
+  end
 end
