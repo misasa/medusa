25d94c5d2a71bbbc45a9ff2f102b18c667ccf735
m-watanabe <m-watanabe@probizmo.co.jp>
2014-02-14 13:20:20 +0900

    system_preferences画面追加

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,4 +1,12 @@
 class Settings < Settingslogic
   source Rails.root.join("config", "application.yml").to_path
   namespace Rails.env
+
+  def self.barcode_type
+    self.barcode['type'] || '2D'
+  end
+
+  def self.barcode_prefix
+    self.barcode['prefix'] || ''
+  end
 end
