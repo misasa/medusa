68ac7b7c3618aaa1d74a796033c63e0dfb217acc
m-imaoka <m-imaoka@probizmo.co.jp>
2014-03-01 16:34:27 +0900

    詳細画面での画像ファイル表示を追加

--- app/decorators/stone_decorator.rb
+++ app/decorators/stone_decorator.rb
@@ -11,6 +11,10 @@ class StoneDecorator < Draper::Decorator
     "/foo/baa/baz/me"
   end
 
+  def primary_picture(width: 250, height: 250)
+    h.image_tag(attachment_files.first.path, width: width, height: height) if attachment_files.present?
+  end
+
   def family_tree
     h.tree(families.group_by(&:parent_id)) do |obj|
       obj.decorate.tree_node(self == obj)
