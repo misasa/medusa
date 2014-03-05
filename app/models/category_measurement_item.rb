bd0eda7603a2534ead229a1fa0d42c26aefd0876
m-watanabe <m-watanabe@probizmo.co.jp>
2014-03-05 15:37:46 +0900

    ソースリファクタリング

--- app/models/category_measurement_item.rb
+++ app/models/category_measurement_item.rb
@@ -1,7 +1,7 @@
 class CategoryMeasurementItem < ActiveRecord::Base
   belongs_to :measurement_item
   belongs_to :measurement_category
-  acts_as_list :scope => :measurement_category, :column => 'position'
+  acts_as_list scope: :measurement_category, column: :position
 
 
   validates :measurement_item, existence: true
