89b831538fc2c37b66fa94c3f002fd329b0ceab7
m-watanabe <m-watanabe@probizmo.co.jp>
2014-03-05 15:06:14 +0900

    不要なコードの削除

--- app/controllers/category_measurement_items_controller.rb
+++ app/controllers/category_measurement_items_controller.rb
@@ -1,7 +1,5 @@
 class CategoryMeasurementItemsController < ApplicationController
-  before_action :find_resource, only: [:destroy]
   load_and_authorize_resource
-  layout "admin"
 
   def move_to_top
     @category_measurement_item.move_to_top
@@ -12,12 +10,6 @@ class CategoryMeasurementItemsController < ApplicationController
     measurement_category = @category_measurement_item.measurement_category
     @category_measurement_item.destroy
     redirect_to edit_measurement_category_path(measurement_category) 
-  end
-
-  private
-
-  def find_resource
-    @category_measurement_item = CategoryMeasurementItem.find(params[:id])
   end
 
 end
