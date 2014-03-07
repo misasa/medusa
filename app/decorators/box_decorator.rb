96c9cc463aba655a3db9e5a165f966b30d9dcee0
Kentaro Matsukawa <k-matsukawa@probizmo.co.jp>
2014-03-07 09:04:04 +0900

    box詳細のat a glance下部を実装

--- app/decorators/box_decorator.rb
+++ app/decorators/box_decorator.rb
@@ -20,4 +20,12 @@ class BoxDecorator < Draper::Decorator
     link = current ? h.content_tag(:strong, name) : name
     h.link_to(link, self)
   end
+
+  def boxed_stones
+    Stone.includes(:record_property, :user, :group, :physical_form).where(box_id: self.id)
+  end
+
+  def boxed_boxes
+    Box.includes(:record_property, :user, :group).where(parent_id: self.id)
+  end
 end
