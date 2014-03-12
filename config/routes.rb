fd640478de67a850a7d799642e4912121389a799
m-watanabe <m-watanabe@probizmo.co.jp>
2014-03-12 18:16:21 +0900

    attachment_file詳細にplaceタグを追加

--- config/routes.rb
+++ config/routes.rb
@@ -112,7 +112,7 @@ Medusa::Application.routes.draw do
     end
     resource :record_property, only: [:show, :update], defaults: { parent_resource: "attachment_file" }
     resources :spots, only: [:index, :create, :update, :destroy], controller: "nested_resources/spots"
-    resources :places, only: [:index, :update, :destroy], controller: "nested_resources/places", defaults: { parent_resource: "attachment_file", association_name: "places" }
+    resources :places, only: [:index, :create, :update, :destroy], controller: "nested_resources/places", defaults: { parent_resource: "attachment_file", association_name: "places" }
     resources :stones, only: [:index, :create,:update, :destroy], controller: "nested_resources/stones", defaults: { parent_resource: "attachment_file", association_name: "stones" }
     resources :boxes, only: [:index, :create, :update, :destroy], controller: "nested_resources/boxes", defaults: { parent_resource: "attachment_file", association_name: "boxes" }
     resources :bibs, only: [:index, :create, :update, :destroy], controller: "nested_resources/bibs", defaults: { parent_resource: "attachment_file" }
