e30f5a7d0907406732d2b144b97d7f6e45644f4f
m-imaoka <m-imaoka@probizmo.co.jp>
2014-03-17 17:12:17 +0900

    GlobalIDを使用してモデル関連を指定できるよう修正

---
+++ config/initializers/belongs_to.rb
@@ -0,0 +1,23 @@
+module ActiveRecord::Associations::Builder
+  class BelongsTo
+    def define_accessors
+      super
+      define_global_id_accessors
+    end
+
+    def define_global_id_accessors
+      mixin.class_eval <<-CODE, __FILE__, __LINE__ + 1
+        def #{name}_global_id
+          association(:#{name}).load_target.try(:global_id)
+        end
+
+        def #{name}_global_id=(id)
+          record = ::RecordProperty.find_by(global_id: id).try!(:datum)
+          if record.is_a? association(:#{name}).klass
+            write_attribute(association(:#{name}).reflection.foreign_key, record.id)
+          end
+        end
+      CODE
+    end
+  end
+end
