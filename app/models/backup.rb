6489fdbdb3952561934b4bef2f42b06998eb4928
m-imaoka <m-imaoka@probizmo.co.jp>
2014-03-18 16:46:01 +0900

    バックアップ処理をsshを用いてリモートサーバにおこなうよう変更

--- app/models/backup.rb
+++ app/models/backup.rb
@@ -3,4 +3,8 @@ require "settingslogic"
 class Backup < Settingslogic
   source File.expand_path("../../../config/application.yml", __FILE__)
   namespace "backup"
+
+  def self.ssh_host
+    "#{username}@#{host}"
+  end
 end
