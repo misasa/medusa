4c5f9a8f89de59ae85fc2d995af639c9e770a68b
m-imaoka <m-imaoka@probizmo.co.jp>
2014-03-18 21:28:15 +0900

    一旦コメントアウト

--- config/initializers/alchemist.rb
+++ config/initializers/alchemist.rb
@@ -1,5 +1,5 @@
 Alchemist.setup
 
-Unit.pluck(:name, :conversion).each do |name, conversion|
-  Alchemist.register(:mass, name.to_sym, 1.to_d / conversion)
-end
+# Unit.pluck(:name, :conversion).each do |name, conversion|
+#   Alchemist.register(:mass, name.to_sym, 1.to_d / conversion)
+# end
