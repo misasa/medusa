02433cc15c8800ebe1387f80e9a8ab275e3c555e
m-imaoka <m-imaoka@probizmo.co.jp>
2014-03-20 10:26:13 +0900

    Unitモデルのバリデーションを修正

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -4,7 +4,7 @@ class Unit < ActiveRecord::Base
   has_many :measurement_categories
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :conversion, presence: true, numericality: true
-  validates :html, presence: true, length: { maximum: 255 }
-  validates :text, presence: true, length: { maximum: 255 }
+  validates :conversion, presence: true, numericality: { only_integer: true }
+  validates :html, presence: true, length: { maximum: 10 }
+  validates :text, presence: true, length: { maximum: 10 }
 end
