2b74d84b12a5caeb69cef9886b4ed02dbc100c0b
Toshiki Ikeuchi <t-ikeuchi@probizmo.co.jp>
2014-03-20 13:24:23 +0900

    box詳細画面を仮実装 refs #91

--- app/decorators/box_decorator.rb
+++ app/decorators/box_decorator.rb
@@ -3,7 +3,7 @@ class BoxDecorator < Draper::Decorator
   delegate_all
 
   def name_with_id
-    h.content_tag(:span, nil, class: "glyphicon glyphicon-cloud") + " #{name} < #{global_id} >"
+    h.content_tag(:span, nil, class: "glyphicon glyphicon-folder-close") + " #{name} < #{global_id} >"
   end
 
   def primary_picture(width: 250, height: 250)
@@ -11,14 +11,41 @@ class BoxDecorator < Draper::Decorator
   end
 
   def family_tree
-    h.tree(families.group_by(&:parent_id)) do |obj|
+    h.tree(current_box_hash(children), parent_id) do |obj|
       obj.decorate.tree_node(self == obj)
     end
   end
-
+  
+  def current_box_hash(children)
+    box_hash = children.group_by(&:parent_id)
+    box_hash[parent_id] = [object]
+    box_hash
+  end
+  
   def tree_node(current=false)
     link = current ? h.content_tag(:strong, name) : name
-    h.link_to(link, self)
+    icon = h.content_tag(:span, nil, class: "glyphicon glyphicon-folder-close")
+    icon + h.link_to(link, self) + stones_count + boxes_count + analyses_count + bibs_count + files_count
+  end
+  
+  def stones_count
+    icon_with_count("cloud", stones.count)
+  end
+  
+  def boxes_count
+    icon_with_count("folder-close", children.count)
+  end
+  
+  def analyses_count
+    #TODO :analysesのカウント数を取得
+  end
+  
+  def bibs_count
+    icon_with_count("book", bibs.count)
+  end
+  
+  def files_count
+    icon_with_count("file", attachment_files.count)
   end
 
   def boxed_stones
@@ -28,4 +55,54 @@ class BoxDecorator < Draper::Decorator
   def boxed_boxes
     Box.includes(:record_property, :user, :group).where(parent_id: self.id)
   end
+  
+  def to_tex
+    lines = []
+    lines << '%------------'
+    lines << 'The sample names and ID of each mounted materials are listed in Table \\ref{mount:materials}.' 
+    lines << '%------------'
+    lines << '\begin{footnotesize}'
+    lines << '\begin{table}'
+    lines << "\\caption{Stones mounted on #{name} (#{global_id}) as of #{Time.now.to_date}.}"
+    lines << '\begin{center}'
+    lines << '\begin{tabular}{lll}'
+    lines << '\hline'
+    lines << ["stone name", "ID", "remark"].join("\t&\t") + "\\\\"
+    lines << '\hline'
+    stones.each do |stone|
+      lines << [stone.name, stone.global_id, ""].join("\t&\t") + "\\\\"
+    end
+    lines << '\hline'
+    lines << '\end{tabular}'
+    lines << '\end{center}'
+    lines << '\label{mount:materials}'
+    lines << '\end{table}'
+    lines << '\end{footnotesize}'
+    lines << '%------------'
+    return lines.join("\n")
+  end
+  
+  def box_path
+    nodes = []
+    if box
+      nodes += box.ancestors.map { |b| box_node(b) }
+    end
+    nodes += [h.content_tag(:span, nil, class: "glyphicon glyphicon-folder-close") + "me"]
+    h.raw(nodes.join("／"))
+  end
+  
+  def analysis_name
+    object.stones.map{|stone| stone.analyses.pluck(:name)}.join(", ")
+  end
+  
+  private
+  
+  def box_node(box)
+    h.content_tag(:span, nil, class: "glyphicon glyphicon-folder-close") + h.link_to(box.name, box)
+  end
+  
+  def icon_with_count(icon, count)
+    h.content_tag(:span, nil, class: "glyphicon glyphicon-#{icon}") + h.content_tag(:span, count) if count.nonzero?
+  end
+  
 end
---
+++ spec/decorators/box_decorator_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe BoxDecorator do
+  
+  describe ".name_with_id" do
+  end
+  
+  describe ".primary_picture" do
+    after { box.primary_picture(width: width, height: height) }
+    let(:box) { FactoryGirl.create(:box).decorate }
+    let(:width) { 250 }
+    let(:height) { 250 }
+    before { box }
+    context "attachment_files is null" do
+      let(:attachment_file_1) { [] }
+      it { expect(helper).not_to receive(:image_tag) }
+    end
+    context "attachment_files is not null" do
+      let(:attachment_file_1) { FactoryGirl.create(:attachment_file, name: "att_1") }
+      before { box.attachment_files << attachment_file_1 }
+      it { expect(helper).to receive(:image_tag).with(attachment_file_1.path, width: width, height: height) }
+    end
+  end
+  
+  describe ".family_tree" do
+  end
+  
+  describe ".tree_node" do
+  end
+  
+  describe ".boxed_stones" do
+  end
+  
+  describe ".boxed_boxes" do
+  end
+  
+  describe ".to_tex" do
+    subject { box.to_tex }
+    let(:box) { FactoryGirl.create(:box).decorate }
+    let(:stone) { FactoryGirl.create(:stone, name: "name_1").decorate }
+    let(:time_now) { Time.now.to_date }
+    before do
+      box
+      box.stones << stone
+      time_now
+    end
+    it { expect(subject).to eq "%------------\nThe sample names and ID of each mounted materials are listed in Table \\ref{mount:materials}.\n%------------\n\\begin{footnotesize}\n\\begin{table}\n\\caption{Stones mounted on box_1 (#{box.global_id}) as of #{time_now}.}\n\\begin{center}\n\\begin{tabular}{lll}\n\\hline\nstone name\t&\tID\t&\tremark\\\\\n\\hline\nname_1\t&\t#{stone.global_id}\t&\t\\\\\n\\hline\n\\end{tabular}\n\\end{center}\n\\label{mount:materials}\n\\end{table}\n\\end{footnotesize}\n%------------" }
+  end
+  
+end
