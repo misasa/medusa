5dc20d25456b2a7d900b0bab65fe74e941416262
m-imaoka <m-imaoka@probizmo.co.jp>
2014-03-20 10:02:42 +0900

    Unitテーブルに合わせてマスタ管理画面を修正

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,46 +3,51 @@ class UnitsController < ApplicationController
   before_action :find_resource, except: [:index, :create]
   load_and_authorize_resource
   layout "admin"
-  
+
   def index
     @search = Unit.search(params[:q])
     @search.sorts = "updated_at ASC" if @search.sorts.empty?
     @units = @search.result.page(params[:page]).per(params[:per_page])
     respond_with @units
   end
-  
+
   def show
     respond_with @unit
   end
-  
+
   def edit
     respond_with @unit
   end
-  
+
   def create
     @unit = Unit.new(unit_params)
     @unit.save
     respond_with(@unit, location: units_path)
   end
-  
+
   def update
     @unit.update_attributes(unit_params)
     respond_with(@unit, location: units_path)
   end
-  
+
   def destroy
     @unit.destroy
     respond_with @unit
   end
-  
+
   private
-  
+
   def unit_params
-    params.require(:unit).permit(:name)
+    params.require(:unit).permit(
+      :name,
+      :conversion,
+      :html,
+      :text
+    )
   end
-  
+
   def find_resource
     @unit = Unit.find(params[:id])
   end
-  
+
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -4,4 +4,7 @@ class Unit < ActiveRecord::Base
   has_many :measurement_categories
 
   validates :name, presence: true, length: { maximum: 255 }
+  validates :conversion, presence: true, numericality: true
+  validates :html, presence: true, length: { maximum: 255 }
+  validates :text, presence: true, length: { maximum: 255 }
 end
