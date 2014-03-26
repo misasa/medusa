72c40fe206cdbdc4bdafb84a11a4115bc6dd1855
Kentaro Matsukawa <k-matsukawa@probizmo.co.jp>
2014-03-26 15:20:17 +0900

    measurement_itemが削除された際は、関連するchemistryレコードも削除するよう修正

--- app/models/measurement_item.rb
+++ app/models/measurement_item.rb
@@ -1,6 +1,6 @@
 class MeasurementItem < ActiveRecord::Base
-  has_many :chemistries
-  has_many :category_measurement_items
+  has_many :chemistries, dependent: :destroy
+  has_many :category_measurement_items, dependent: :destroy
   has_many :measurement_categories, through: :category_measurement_items
   belongs_to :unit
 
