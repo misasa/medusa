1147e23a216ee4518ece251649d1d322bf0f05d2
m-imaoka <m-imaoka@probizmo.co.jp>
2014-03-26 15:55:35 +0900

    検索ダイアログ仮実装

---
+++ app/assets/javascripts/search_modal.js
@@ -0,0 +1,27 @@
+(function($) {
+
+  var input;
+
+  function determine() {
+    $(input).val($(this).data("id"));
+    $("#search-modal").modal("hide");
+    return false;
+  }
+
+  $(document).on("click.bs.modal.data-api", "[data-toggle='modal']", function() {
+    if($(this).data("target") === "#search-modal") {
+      input = $(this).data("input");
+    } else {
+      input = undefined;
+    }
+  });
+
+  $(document).on("ajax:success", "#search-modal", function(event, data, status) {
+    $(this).find("div.modal-content").html(data);
+  });
+
+  $(document).on("loaded.bs.modal", "#search-modal", function() {
+    $(this).on("click", ".determine", determine);
+  });
+
+}) (jQuery);
