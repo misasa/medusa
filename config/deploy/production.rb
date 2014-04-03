133337ae917133e1c34dc8ecf5f9766d25daa6be
Kentaro Matsukawa <k-matsukawa@probizmo.co.jp>
2014-04-03 17:20:08 +0900

    運用時のサブディレクトリ名を/appにする refs #98

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,6 +16,8 @@
 # extended properties on the server.
 server 'devel.misasa.okayama-u.ac.jp', user: 'medusa', roles: %w{web app db}, ssh_options: { keys: %w{/home/probizmo/.ssh/id_rsa} }
 
+set :relative_url_root, "/app"
+
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
