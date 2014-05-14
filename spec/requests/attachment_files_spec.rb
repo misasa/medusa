0338a6c8d20f8a8d35aea4ec3e12d88be0bbc375
Toshiki Ikeuchi <t-ikeuchi@probizmo.co.jp>
2014-05-14 14:27:30 +0900

    attachment_fileの一覧画面とat-a-glance画面のpdfアイコンを修正 refs #122

--- spec/requests/attachment_files_spec.rb
+++ spec/requests/attachment_files_spec.rb
@@ -22,7 +22,31 @@ describe "attachment_file" do
     context "data_content_type is jpeg" do
       let(:data_content_type) { "image/jpeg" }
       it "do not show pdf icon" do
-        expect(page).not_to have_link("file-#{attachment_file.id}-button")
+        expect(page).to have_no_link("file-#{attachment_file.id}-button")
+      end
+    end
+  end
+  
+  describe "stone detail screen" do
+    before { click_link(attachment_file.data_file_name) }
+    let(:create_data) { attachment_file.create_record_property(user_id: login_user.id) }
+    let(:attachment_file) { FactoryGirl.create(:attachment_file, data_file_name: "file_name", data_content_type: data_content_type, original_geometry: "", affine_matrix: []) }
+    describe "at-a-glance tab" do
+      before { click_link("at-a-glance") }
+      describe "pdf icon" do
+        context "data_content_type is pdf" do
+          let(:data_content_type) { "application/pdf" }
+          it "show pdf icon" do
+            save_page
+            expect(page).to have_link("file-#{attachment_file.id}-button")
+          end
+        end
+        context "data_content_type is jpeg" do
+          let(:data_content_type) { "image/jpeg" }
+          it "do not show pdf icon" do
+            expect(page).to have_no_link("file-#{attachment_file.id}-button")
+          end
+        end
       end
     end
   end
