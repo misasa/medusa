46318674a8aa1357b83526929ccc8260a8111cc7
Yusuke Yachi <yusuke.yachi@gmail.com>
2015-03-02 14:15:36 +0900

    measurment_category nicknames order refs #383

--- app/models/measurement_category.rb
+++ app/models/measurement_category.rb
@@ -1,6 +1,6 @@
 class MeasurementCategory < ActiveRecord::Base
   has_many :category_measurement_items, dependent: :destroy
-  has_many :measurement_items, through: :category_measurement_items
+  has_many :measurement_items, through: :category_measurement_items, :order => 'category_measurement_items.position'
   belongs_to :unit
 
   validates :name, presence: true, length: {maximum: 255}, uniqueness: :name
