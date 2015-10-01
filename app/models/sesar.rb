291b135cb1f5631bb9916911c19270b36b2a35e1
m-imaoka <m-imaoka@probizmo.co.jp>
2015-10-01 17:47:17 +0900

    Implement registration request. (there remains TODO.)

--- app/models/sesar.rb
+++ app/models/sesar.rb
@@ -2,6 +2,9 @@ require "active_resource"
 
 class Sesar < ActiveResource::Base
   self.site = "http://app.geosamples.org/"
+  # TODO: ユーザとパスワードは外部ファイルから取得すべき
+  self.user = "m-imaoka@probizmo.co.jp"
+  self.password = "probizmo"
   self.prefix = "/sample/"
   self.element_name = "sample"
   self.collection_name = "igsn"
@@ -21,5 +24,31 @@ class Sesar < ActiveResource::Base
   rescue ActiveResource::Redirection => e
     # 旧URLへのリダイレクト対策
     super(:one, from: e.response["location"])
+  end
+
+  def save
+    response = connection.post("/webservices/upload.php", "username=#{self.class.user}&password=#{self.class.password}&content=#{encode}&submit=submit", "Content-Type" => "application/x-www-form-urlencoded")
+    self.igsn = Hash.from_xml(response.body)["results"]["sample"]["igsn"]
+    true
+  rescue ActiveResource::BadRequest => e
+    # TODO: 失敗した理由をレスポンスXMLから取得してユーザに通知すべき
+    false
+  end
+
+  def to_xml(options ={})
+    xml = ""
+    builder = Builder::XmlMarkup.new(target: xml)
+    xml.clear
+    builder.instruct!
+    builder.samples("xmlns" => "http://app.geosamples.org", "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance", "xsi:schemaLocation" => "http://app.geosamples.org/samplev2.xsd") do |samples|
+      samples.sample do |sample|
+        # TODO: 設定内容はダミー
+        sample.user_code "IEIMA"
+        sample.sample_type "Individual Sample"
+        sample.name "test"
+        sample.material "Rock"
+      end
+    end
+    xml
   end
 end
