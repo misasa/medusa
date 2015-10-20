ede8babee843f22c252641a9bffff3adb34233c5
m-imaoka <m-imaoka@probizmo.co.jp>
2015-10-20 17:39:37 +0900

    add assign position callback to table_stones.

--- app/models/table_stone.rb
+++ app/models/table_stone.rb
@@ -3,4 +3,11 @@ class TableStone < ActiveRecord::Base
   belongs_to :table
   belongs_to :stone
 
+  before_save :assign_position
+
+  private
+
+  def assign_position
+    self.position = (self.class.where(table_id: table_id).maximum(:position) || 0) + 1
+  end
 end
