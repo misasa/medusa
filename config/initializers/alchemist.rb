6bd3522af62d0f59faf9b215d1272449162b5aed
Kentaro Matsukawa <k-matsukawa@probizmo.co.jp>
2015-11-06 10:37:55 +0900

    単位変換定義にa,Ka,Ma,Gaを追加

--- config/initializers/alchemist.rb
+++ config/initializers/alchemist.rb
@@ -5,3 +5,8 @@ if Unit.attribute_method?(:name) && Unit.attribute_method?(:conversion)
     Alchemist.register(:mass, name.to_sym, 1.to_d / conversion)
   end
 end
+
+Alchemist.register(:time, [:annum, :annums, :a], 1.year)
+Alchemist.register(:time, [:kiloannum, :kiloannums, :Ka], 1000.year)
+Alchemist.register(:time, [:megaannum, :megaannums, :Ma], (1000**2).year)
+Alchemist.register(:time, [:gigaannum, :gigaannums, :Ga], (1000**3).year)
