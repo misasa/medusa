d828f1fc0a58f536dc664562e40a7eaeeb0d5db7
Kentaro Matsukawa <k-matsukawa@probizmo.co.jp>
2015-11-26 14:08:44 +0900

    chemistriesテーブルの単位IDにNOTNULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125084856) do
+ActiveRecord::Schema.define(version: 20151126060654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,7 +130,7 @@ ActiveRecord::Schema.define(version: 20151125084856) do
     t.float    "uncertainty"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "unit_id"
+    t.integer  "unit_id",             null: false
   end
 
   add_index "chemistries", ["analysis_id"], name: "index_chemistries_on_analysis_id", using: :btree
