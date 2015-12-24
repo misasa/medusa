aa6c7ee35056d7a3317b2fa556afc6702e10741d
Yusuke Yachi <yusuke.yachi@gmail.com>
2015-12-24 16:15:07 +0900

    table at-a-glance interface

--- app/decorators/table_decorator.rb
+++ app/decorators/table_decorator.rb
@@ -1,6 +1,12 @@
 class TableDecorator < Draper::Decorator
   delegate_all
 
+  def bib_name_with_id
+  	return unless bib
+    h.content_tag(:span, nil, class: "glyphicon glyphicon-book") + " #{bib.name} < #{bib.global_id} >"
+  end
+
+
   def name_with_id
     h.content_tag(:span, nil, class: "glyphicon glyphicon-th-list") + " #{description} < #{global_id} >"
   end
