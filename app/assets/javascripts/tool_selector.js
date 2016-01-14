4397d39bb53dbaf0365d909d84db1265a09cce81
m-imaoka <m-imaoka@probizmo.co.jp>
2016-01-14 16:06:33 +0900

    when xhr abort, not display fatal error message.

--- app/assets/javascripts/tool_selector.js
+++ app/assets/javascripts/tool_selector.js
@@ -3,6 +3,10 @@
     $("div.tool-content").html(data);
   });
   $(document).on("ajax:error", "div.tool-selector", function(event, data, status) {
+    // ignroe abort
+    if (data.readyState === 0 && data.status === 0) {
+      return;
+    }
     $.notification.error("Fatal error.");
   });
   $(document).ready(function() {
