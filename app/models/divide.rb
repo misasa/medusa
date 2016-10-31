c7c5d036453a6cae423730e431233dfc48307ffe
ke-matsumoto <ke-matsumoto@probizmo.co.jp>
2016-10-31 15:56:37 +0900

    Fix アソシエーションを定義

--- app/models/divide.rb
+++ app/models/divide.rb
@@ -1,14 +1,11 @@
 class Divide < ActiveRecord::Base
   belongs_to :before_specimen_quantity, class_name: "SpecimenQuantity"
+  has_one :before_specimen, through: :before_specimen_quantity, source: :specimen
   has_many :specimen_quantities
 
   default_scope -> { order(:updated_at) }
 
   scope :specimen_id_is, -> (id) { includes(:before_specimen_quantity).where(specimen_quantities: { specimen_id: id }) }
-
-  def before_specimen
-    before_specimen_quantity.try(:specimen)
-  end
 
   def chart_updated_at
     (updated_at.to_i + 9.hours) * 1000
