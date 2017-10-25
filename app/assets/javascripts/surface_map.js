b95212d12b3498109065e98b20569104393747b9
m-imaoka <m-imaoka@probizmo.co.jp>
2017-10-25 15:16:09 +0900

    implement slider control for radius size.

--- app/assets/javascripts/surface_map.js
+++ app/assets/javascripts/surface_map.js
@@ -1,5 +1,6 @@
 function initSurfaceMap() {
   var div = document.getElementById("surface-map");
+  var radiusSelect = document.getElementById("spot-radius");
   var baseUrl = div.dataset.baseUrl;
   var global_id = div.dataset.globalId;
   var attachment_files = JSON.parse(div.dataset.attachmentFiles);
@@ -7,7 +8,6 @@ function initSurfaceMap() {
   var layers = [];
   var baseMaps = {};
   var overlayMaps = {};
-  var circles = [];
   var x = 256;
   var y = 256;
   var zoom = 1;
@@ -27,26 +27,17 @@ function initSurfaceMap() {
     
   var circlesLayer = L.layerGroup();
   for(spot of spots) {
-    var circle = L.circle([-spot.y, spot.x], {
+    var circle = L.circleMarker([-spot.y, spot.x], {
       color: 'red',
       fillColor: '#f03',
       fillOpacity: 0.5,
-      radius: 5,
+      radius: 3,
     });
     circle.id = spot.id;
     circle.on("click", function() {
-      target = this;
-      $.get("/spots/" + this.id + "/analysis", {}, null, "json").done(function(data) {
-	if(data) {
-          var text = data.name;
-	  $.each(data.chemistries, function(idx, chemistry) {
-            text = text + "<br />" + chemistry.measurement_item.nickname + ": " + chemistry.value + chemistry.unit.html;
-	  });
-          target.bindPopup(text, { maxHeight: 50 }).openPopup();
-        }
-      });
+      var latlng = this.getLatLng();
+      this.bindPopup("lat: " + latlng.lat + "<br />lng: " + latlng.lng).openPopup();
     });
-    circles.push(circle);
     circle.addTo(circlesLayer);
   }
   layers.push(circlesLayer);
@@ -57,6 +48,40 @@ function initSurfaceMap() {
     crs: L.CRS.Simple,
     layers: layers
   });
+
+  var radiusControl = L.control({position: 'bottomright'});
+  radiusControl.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'leaflet-control-layers'),
+        range = L.DomUtil.create('input');
+    range.type = 'range';
+    range.min = 1;
+    range.max = 10;
+    range.value = 3;
+    range.style = 'width:60px;margin:3px;';
+    div.appendChild(range);
+    L.DomEvent.on(range, 'change', function() {
+      circlesLayer.eachLayer(function(layer) {
+        layer.setRadius(range.value);
+      });
+    });
+    L.DomEvent.on(range, 'input', function() {
+      circlesLayer.eachLayer(function(layer) {
+        layer.setRadius(range.value);
+      });
+    });
+    L.DomEvent.on(range, 'mouseenter', function(e) {
+      map.dragging.disable()
+    });
+    L.DomEvent.on(range, 'mouseleave', function(e) {
+      map.dragging.enable();
+    });
+    return div;
+  };
+  radiusControl.addTo(map);
+
+  L.control.scale({ imperial: false }).addTo(map);
+
   L.control.layers(baseMaps, overlayMaps).addTo(map);
+
   map.setView(map.unproject([x, y], zoom), zoom);
 }
