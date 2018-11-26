f8e8b77d64fcb79a7b5625e4a1ba76168e067c85
Yusuke Yachi <yyachi@misasa.okayama-u.ac.jp>
2018-11-26 11:36:55 +0900

    change default'

--- app/assets/javascripts/surface_map.js
+++ app/assets/javascripts/surface_map.js
@@ -141,7 +141,7 @@ function initSurfaceMap() {
 	L.tileLayer(baseUrl + global_id + '/' + id + '/{z}/{x}_{y}.png', { opacity: opacity }).addTo(group);
       });
       layers.push(group);
-      if (name === "") { name = "not layered"; }
+      if (name === "") { name = "default"; }
       overlayMaps[name] = group;
     }
   });
