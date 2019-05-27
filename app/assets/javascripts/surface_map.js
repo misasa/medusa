1274d71b6f8a018ee58d4d226678a7fbe9969a99
yyachi <yusuke.yachi@gmail.com>
2019-05-27 20:00:59 +0900

    layer order control

--- app/assets/javascripts/surface_map.js
+++ app/assets/javascripts/surface_map.js
@@ -1,29 +1,211 @@
 // OpacityLayerControl
 L.Control.OpacityLayers = L.Control.Layers.extend({
+    onAdd: function (map) {
+      this._initLayout();
+      this._update();
+
+      this._map = map;
+      map.on('zoomend', this._checkDisabledLayers, this);
+      map.on('changeorder', this._onLayerChange, this);
+      for (var i = 0; i < this._layers.length; i++) {
+        this._layers[i].layer.on('add remove', this._onLayerChange, this);
+      }
+
+      return this._container;
+    },
+    onRemove: function () {
+      this._map.off('zoomend', this._checkDisabledLayers, this);
+      this._map.off('changeorder', this._onLayerChange, this);
+      for (var i = 0; i < this._layers.length; i++) {
+        this._layers[i].layer.off('add remove', this._onLayerChange, this);
+      }
+    },
+    _initLayout: function(){
+        L.Control.Layers.prototype._initLayout.call(this);
+        base = $(this._container).find(".leaflet-control-layers-base");
+        overlays = $(this._container).find(".leaflet-control-layers-overlays");
+        separator = $(this._container).find(".leaflet-control-layers-separator");
+        overlays.after(separator);
+        separator.after(base);
+    },
+    _addLayer: function (layer, name, overlay) {
+        if (this._map) {
+          layer.on('add remove', this._onLayerChange, this);
+        }
+
+        this._layers.unshift({
+          layer: layer,
+          name: name,
+          overlay: overlay
+        });
+
+        if (this.options.sortLayers) {
+          this._layers.sort(Util.bind(function (a, b) {
+            return this.options.sortFunction(a.layer, b.layer, a.name, b.name);
+          }, this));
+        }
+
+        if (this.options.autoZIndex && layer.setZIndex) {
+          this._lastZIndex++;
+          layer.setZIndex(this._lastZIndex);
+        }
+        this._expandIfNotCollapsed();
+    },
+    _update: function() {
+        if (!this._container) { return this; }
+
+        L.DomUtil.empty(this._baseLayersList);
+        L.DomUtil.empty(this._overlaysList);
+
+        this._layerControlInputs = [];
+        var baseLayersPresent, overlaysPresent, i, obj, baseLayersCount = 0;
+
+        for (i = 0; i < this._layers.length; i++) {
+          obj = this._layers[i];
+          this._addItem(obj);
+	  overlaysPresent = overlaysPresent || obj.overlay;
+	  baseLayersPresent = baseLayersPresent || !obj.overlay;
+          baseLayersCount += !obj.overlay ? 1 : 0;
+        }
+
+        // Hide base layers section if there's only one layer.
+        if (this.options.hideSingleBase) {
+          baseLayersPresent = baseLayersPresent && baseLayersCount > 1;
+          this._baseLayersList.style.display = baseLayersPresent ? '' : 'none';
+        }
+        this._separator.style.display = overlaysPresent && baseLayersPresent ? '' : 'none';
+        return this;
+    },
     _addItem: function (obj) {
-        var label = L.Control.Layers.prototype._addItem.call(this, obj);
+	//var row = L.DomUtil.create('div','leaflet-row');
+        var label = document.createElement('label'),
+            checked = this._map.hasLayer(obj.layer),
+            input;
 
-        //if (!obj.overlay || !obj.layer.setOpacity) {
-	if (!obj.overlay) {
-            return label;
-        }
-
-        input = document.createElement('input');
-        input.type = 'range';
-        input.min = 0;
-        input.max = 100;
-        input.value = 100;
-        input.layerId = L.stamp(obj.layer);
-        if (this._map.hasLayer(obj.layer)) {
-            input.style.display = 'block';
+        if (obj.overlay) {
+          input = document.createElement('input');
+          input.type = 'checkbox';
+          input.className = 'leaflet-control-layers-selector';
+          input.defaultChecked = checked;
         } else {
-            input.style.display = 'none';
+          input = this._createRadioElement('leaflet-base-layers', checked);
         }
 
-        L.DomEvent.on(input, 'change', this._onInputClick, this);
+        this._layerControlInputs.push(input);
+        input.layerId = L.Util.stamp(obj.layer);
 
-        label.appendChild(input);
+        L.DomEvent.on(input, 'click', this._onInputClick, this);
+
+        var name = document.createElement('span');
+        name.innerHTML = ' ' + obj.name;
+        //var col = L.DomUtil.create('div','leaflet-input');
+        //col.appendChild(input);
+        //row.appendChild(col);
+        //var col = L.DomUtil.create('div', 'leaflet-name');
+        //label.htmlFor = input.id;
+        //col.appendChild(label);
+        //row.appendChild(col);
+        //label.appendChild(name);
+        // Helps from preventing layer control flicker when checkboxes are disabled
+        // https://github.com/Leaflet/Leaflet/issues/2771
+        var holder = document.createElement('div');
+        label.appendChild(holder);
+        holder.appendChild(input);
+        holder.appendChild(name);
+
+        if (obj.overlay) {
+          var up = L.DomUtil.create('div','leaflet-up');
+          L.DomEvent.on(up, 'click', this._onUpClick, this);
+          up.layerId = input.layerId;
+          holder.appendChild(up);
+          var down = L.DomUtil.create('div','leaflet-down');
+          L.DomEvent.on(down, 'click', this._onDownClick, this);
+          down.layerId = input.layerId;
+          holder.appendChild(down);
+
+          input = document.createElement('input');
+          input.type = 'range';
+          input.min = 0;
+          input.max = 100;
+          if (obj.layer && obj.layer.getLayers() && obj.layer.getLayers()[0]){
+	      input.value = 100 * obj.layer.getLayers()[0].options.opacity
+          } else {
+            input.value = 100;
+          }
+          input.layerId = L.stamp(obj.layer);
+          if (this._map.hasLayer(obj.layer)) {
+              input.style.display = 'block';
+          } else {
+              input.style.display = 'none';
+          }
+
+          L.DomEvent.on(input, 'change', this._onInputClick, this);
+
+          label.appendChild(input);
+
+          
+        }
+
+        var container = obj.overlay ? this._overlaysList : this._baseLayersList;
+        container.appendChild(label);
+
+        this._checkDisabledLayers();
+
+
         return label;
+    },
+    _onUpClick: function(e){
+      var layerId = e.currentTarget.layerId;
+      var obj = this._getLayer(layerId);
+      if(!obj.overlay){
+        return;
+      }
+      replaceLayer = null;
+      var zidx = this._getZIndex(obj);
+      for(var i=0; i < this._layers.length; i++){
+         ly = this._layers[i];
+         var auxIdx = this._getZIndex(ly);
+         if(ly.overlay && (zidx + 1) === auxIdx){
+           replaceLayer = ly;
+           break;
+         }
+      }
+
+      var newZIndex = zidx + 1;
+      if(replaceLayer){
+        obj.layer.setZIndex(newZIndex);
+        replaceLayer.layer.setZIndex(newZIndex - 1);
+        this._layers.splice(i,1);
+        this._layers.splice(i+1,0,replaceLayer);
+        this._map.fire('changeorder', obj, this);
+      }
+    },
+    _onDownClick: function(e){
+      var layerId = e.currentTarget.layerId;
+      var obj = this._getLayer(layerId);
+      if(!obj.overlay){
+        return;
+      }
+      replaceLayer = null;
+      var zidx = this._getZIndex(obj);
+      for(var i=0; i < this._layers.length; i++){
+         ly = this._layers[i];
+         layerId = L.Util.stamp(ly.layer);
+         var auxIdx = this._getZIndex(ly);
+         if(ly.overlay && (zidx - 1) === auxIdx){
+           replaceLayer = ly;
+           break;
+         }
+      }
+
+      var newZIndex = zidx - 1;
+      if(replaceLayer){
+        obj.layer.setZIndex(newZIndex);
+        replaceLayer.layer.setZIndex(newZIndex + 1);
+        this._layers.splice(i,1);
+        this._layers.splice(i-1,0,replaceLayer);
+        this._map.fire('changeorder', obj, this);
+      }
     },
     _onInputClick: function () {
         var i, input, obj,
@@ -40,12 +222,13 @@ L.Control.OpacityLayers = L.Control.Layers.extend({
             
             if (input.type == 'range' && this._map.hasLayer(obj.layer)) {
                 input.style.display = 'block';
+                opacity = input.value / 100.0;
 		group_layers = obj.layer.getLayers();
 		for (var j = 0; j < group_layers.length; j++){
 		    var _layer = group_layers[j];
 		    if (typeof _layer._url === 'undefined'){
 		    } else {
-			_layer.setOpacity(input.value / 100.0);
+			_layer.setOpacity(opacity);
 		    }
 		}
                 continue;
@@ -65,7 +248,20 @@ L.Control.OpacityLayers = L.Control.Layers.extend({
         this._handlingClick = false;
 
         this._refocusOnMap();
-    }
+	},
+        _getZIndex: function(ly){
+	  var zindex = 9999999999;
+          if(ly.layer.options && ly.layer.options.zIndex){
+	      zindex = ly.layer.options.zIndex;
+          } else if (ly.layer.getLayers && ly.layer.eachLayer){
+              ly.layer.eachLayer(function(lay){
+	        if(lay.options && lay.options.zIndex){
+		    zindex = Math.min(lay.options.zIndex, zindex);
+                }
+              });
+          }
+          return zindex;
+        } 
 });
 
 L.control.opacityLayers = function (baseLayers, overlays, options) {
@@ -236,7 +432,7 @@ function initSurfaceMap() {
   var spotsLayer = L.layerGroup.spots(map, spots, urlRoot);
   map.addLayer(spotsLayer);
 
-  overlayMaps['grid'] = L.layerGroup.grid(map, length);
+  //overlayMaps['grid'] = L.layerGroup.grid(map, length);
 
   var radiusControl = L.control.radius(spotsLayer, {position: 'bottomright'}).addTo(map);
 
@@ -257,7 +453,7 @@ function initSurfaceMap() {
   L.control.surfaceScale({ imperial: false, length: length }).addTo(map);
 
   //L.control.layers(baseMaps, overlayMaps).addTo(map);
-  L.control.opacityLayers(baseMaps, overlayMaps).addTo(map);
+  L.control.opacityLayers(baseMaps, overlayMaps, {hideSingleBase: false}).addTo(map);
   map.setView(map.unproject([256 / 2, 256 / 2], 0), zoom);
 
   map.addControl(new L.Control.Fullscreen());
