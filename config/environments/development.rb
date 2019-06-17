43b9afe657538131bd75309b1464b4094e512a3b
yyachi <yusuke.yachi@gmail.com>
2019-06-17 14:32:54 +0900

    mv stackprof from config.ru to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,20 @@ Medusa::Application.configure do
   #  Bullet.add_footer = false
   #end
   
+  is_stackprof = ENV['ENABLE_STACKPROF'].to_i.nonzero?
+  stackprof_mode = (ENV['STACKPROF_MODE'] || :wall).to_sym
+  stackprof_interval = (ENV['STACKPROF_INTERVAL'] || 1000).to_i
+  stackprof_save_every = (ENV['STACKPROF_SAVE_EVERY'] || 5).to_i
+  stackprof_path = ENV['STACKPROF_PATH'] || 'tmp/stackprof/wall_mode.dump'
+
+  config.middleware.use StackProf::Middleware,
+    enabled: is_stackprof,
+    mode: stackprof_mode,
+    raw: true,
+    interval: stackprof_interval,
+    save_every: stackprof_save_every,
+    path: stackprof_path  
+
   #RackLineprf
   config.middleware.use Rack::Lineprof
 end
