d1a061303058ba915d12b8fdc458982da2237877
yyachi <yusuke.yachi@gmail.com>
2019-06-25 09:32:28 +0900

    merge layer function

--- app/models/surface_layer.rb
+++ app/models/surface_layer.rb
@@ -27,4 +27,46 @@ class SurfaceLayer < ActiveRecord::Base
     end
     [left, upper, right, bottom]
   end
+ 
+  def tile_dir
+    File.join(surface.map_dir, "layeres","#{id}")
+  end
+
+  def tile_image_path(z,x,y, opts = {})
+    extension = opts[:extension] || 'png'
+    File.join(tile_dir,z.to_s,"#{x}_#{y}.#{extension}")
+  end
+
+  def clean_tiles(options = {})
+    if Dir.exists?(tile_dir)
+      line = Terrapin::CommandLine.new("rm", "-r :tile_dir", logger: logger)
+      line.run(tile_dir: tile_dir)
+    end
+  end
+
+  def merge_images
+    clean_tiles
+    surface_images.to_a[0..2].reverse.each do |surface_image|
+      puts "#{surface_image.image_id}"
+      0.upto(surface_image.original_zoom_level) do |zoom|
+        target_dir = File.join(tile_dir, "#{zoom}")
+        unless Dir.exists?(target_dir)
+          line = Terrapin::CommandLine.new("mkdir", "-p :dir", logger: logger)
+          line.run(dir: File.join(target_dir))
+        end  
+        surface_image.tiles_each(zoom) do |x, y|
+          src_path = surface_image.tile_image_path(zoom,x,y)
+          dest_path = tile_image_path(zoom,x,y)
+          if File.exists?(dest_path)
+            line = Terrapin::CommandLine.new("composite", "-compose over :dest :src :dest")
+            line.run(src: src_path, dest: dest_path)
+          else
+            line = Terrapin::CommandLine.new("cp", ":src :dest", logger: logger)
+            line.run(src: src_path, dest: dest_path)
+          end
+        end
+      end
+    end
+  end
+
 end
