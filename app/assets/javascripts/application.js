3e0e8f53bc76b62227a35ea3cd75d4205bd337dc
yyachi <yusuke.yachi@gmail.com>
2019-07-05 18:50:44 +0900

    mv require statement for zoomlabel from application.js to surface_map.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,6 @@
 //= require highcharts/highcharts
 //= require highcharts/highcharts-more
 //= require highcharts/highstock
-//= require Leaflet.ZoomLabel/src/L.Control.ZoomLabel
 function basename(path) {
   return path.substr(0, path.lastIndexOf("."));
 }
