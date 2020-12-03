bb3f9f3ea1d57871f1b96a08b76a2f95ba703dd8
yyachi <yusuke.yachi@gmail.com>
2020-12-03 16:01:24 +0900

    leaflet-view-meta

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,7 @@
 //= require leaflet/dist/leaflet
 //= require leaflet.glify/dist/glify-browser.js
 //= require leaflet-toolbar/dist/leaflet.toolbar.js
+//= require leaflet-view-meta/src/leaflet-view-meta.js
 //= require leaflet-distortableimage/dist/leaflet.distortableimage.js
 //= require leaflet-fullscreen/dist/Leaflet.fullscreen.js
 //= require Leaflet.Spin/leaflet.spin.min.js
