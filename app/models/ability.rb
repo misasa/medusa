365c2092b5ba8437c12bbf8e9341570d2c6707ab
Yusuke Yachi <yusuke.yachi@gmail.com>
2021-03-24 17:06:54 +0900

    fixed bug: guest can not show published records

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,15 +3,17 @@ class Ability
 
   def initialize(user)
     alias_action :pmlame, :family, :picture, :map, :download_label, :download_card, :download, to: :read
-
     if user.admin?
       can :manage, :all
     end
-
-    can :manage, [Surface, Table, AttachmentFile, Bib, Box, Analysis, Chemistry, Place, Spot, Specimen] do |record|
+    models = [Surface, Table, AttachmentFile, Bib, Box, Analysis, Chemistry, Place, Spot, Specimen]
+    decorators = models.map{|model| Object.const_get(model.to_s + 'Decorator')}
+    models_with_decorators = models.concat(decorators)
+    can :manage, models_with_decorators do |record|
       record.writable?(user)
     end
-    can :read, [Surface, Table, AttachmentFile, Bib, Box, Analysis, Chemistry, Place, Spot, Specimen] do |record|
+
+    can :read, models_with_decorators do |record|
       record.readable?(user)
     end
 
